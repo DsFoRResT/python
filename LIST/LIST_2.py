7ca933a3982cfffece14b4720501a9213e77c4f9
DsFoRResT <sardorcoc@gmail.com>
2022-09-09 22:31:02 +0500

    ds

---
+++ LIST/LIST_2.py
@@ -0,0 +1,26 @@
+"""
+myList = []
+a = "Sardor"
+myList.append(a)
+b = "Aziz"
+myList.append(b)
+c = "Kamola"
+myList.append(c)
+
+# myList o'z elementlarini x ga topshiradi
+for x in myList:
+    print(x)
+"""
+"""
+myList = []
+a = "Sardor"
+myList.append(a)
+b = "Aziz"
+myList.append(b)
+c = "Kamola"
+myList.append(c)
+
+
+for i in range(len(myList)):
+    print(myList[i])
+"""
