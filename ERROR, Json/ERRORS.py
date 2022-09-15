3a6a5abfea7397e00f60cecde9bfaf6aa70f4844
DsFoRResT <sardorcoc@gmail.com>
2022-09-15 08:41:29 +0500

    ds

---
+++ ERROR, Json/ERRORS.py
@@ -0,0 +1,52 @@
+"""
+a = ind ( input () )
+b = int ( inut () )
+c = a + b
+print ( c )
+"""
+"""
+print "Hello World"
+"""
+"""
+a = 5
+b = 5
+c = 10 / (a - b)
+print ( c )
+"""
+"""
+a = "Salom Sardor
+print ( a )
+"""
+"""
+a = 5
+b = 6
+c = max( a , b
+print( c )
+"""
+"""
+for i in range(10):
+print ( i )
+"""
+"""
+s = "salom"
+print( s**2 )
+"""
+"""
+s = "salom"
+print( s[10] )
+"""
+"""
+num1 = 10
+num2 = 20
+summ = num1 + num
+print( summ )
+"""
+"""
+a = int (input ( ) ) #5.6 kiriting
+print(a)
+"""
+"""
+r = 10
+s = r * 3.14  # 3.14 deb yozish xato pi sonini aniq qiymatini yozish kerak
+print ( s )
+"""
---
+++ ERROR, Json/finish.py
@@ -0,0 +1,15 @@
+import json
+
+# a Python object (dict):
+x = {
+  "name": "John",
+  "age": 30,
+  "pasport" : True,
+  "city": "New York"
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print(y)
